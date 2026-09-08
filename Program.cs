d66db68708fab47e1aaf29c26d61f803490b6b58
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-08 21:32:27 +0300

    Lab01 Task05

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace oop_course
             System.Threading.Thread.CurrentThread.CurrentCulture =
                 System.Globalization.CultureInfo.InvariantCulture;
 
-            // Тепер запускаємо четверту задачу
-            Task4.Run();
+            // Тепер запускаємо п'яту задачу
+            Task5.Run();
         }
     }
 }
