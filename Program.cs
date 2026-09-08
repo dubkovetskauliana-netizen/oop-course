4ee087eca72b354333b050c7c376b48e11c71d66
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-08 21:02:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace oop_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Налаштування для крапки в дробових числах
+            System.Threading.Thread.CurrentThread.CurrentCulture =
+                System.Globalization.CultureInfo.InvariantCulture;
+
+            // Запуск нашого першого завдання
+            Task1.Run();
+        }
+    }
+}
