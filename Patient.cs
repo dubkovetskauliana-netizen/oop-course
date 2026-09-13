802e9623f879cecc5a8fe93cf1807dd5ec8d549c
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-13 18:34:12 +0300

    Lab03 Task01

---
+++ Patient.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ClinicApp
+{
+    public class Patient
+    {
+        private static int _nextId = 1;
+
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string BloodType { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Today.Year - DateOfBirth.Year;
+                if (DateOfBirth.Date > DateTime.Today.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public bool IsAdult => Age >= 18;
+
+        public Patient() : this("Невідомий", "Пацієнт", DateTime.Today.AddYears(-26), "Невідомо", "0000000000", "")
+        {
+        }
+
+        public Patient(string firstName, string lastName) : this(firstName, lastName, DateTime.Today.AddYears(-26), "Невідомо", "0000000000", "")
+        {
+        }
+
+        public Patient(string firstName, string lastName, DateTime dateOfBirth, string bloodType, string phone, string email = "")
+        {
+            Id = _nextId++;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            BloodType = bloodType;
+            Phone = phone;
+            Email = email;
+        }
+
+        public string GetAgeCategory()
+        {
+            int age = Age;
+            if (age < 18)
+            {
+                return "дитина";
+            }
+            else if (age < 60)
+            {
+                return "дорослий";
+            }
+            else
+            {
+                return "літній";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {FullName} | Вік: {Age} ({GetAgeCategory()}) | Кров: {BloodType} | Тел: {Phone}";
+        }
+    }
+}
