c17aa83bd79c933cab6c0c5575986177e4bf5ecf
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-13 17:37:55 +0300

    Lab02 Task06

--- Program.cs
+++ Program.cs
@@ -3,4 +3,4 @@
 System.Threading.Thread.CurrentThread.CurrentCulture =
     System.Globalization.CultureInfo.InvariantCulture;
 
-Task5.Run();
+Task6.Run();
--- Task6.cs
+++ Task6.cs
@@ -1,37 +1,48 @@
 ﻿using System;
 
-namespace oop_course
+namespace Lab02
 {
-    public static class Task6
+    public class Task6
     {
         public static void Run()
         {
-            // зчитуємо 5-6 значний номер медичної картки
-            Console.Write("Введіть номер картки: ");
-            string input = Console.ReadLine()!;
-            int num = int.Parse(input);
+            int n = int.Parse(Console.ReadLine()!);
+            int[][] doctors = new int[n][];
 
-            // знаходимо останню цифру числа за допомогою % 10
-            int lastDigit = num % 10;
-
-            // визначаємо відділення за допомогою switch expression та патерну or
-            string dep = lastDigit switch
+            for (int i = 0; i < n; i++)
             {
-                0 or 1 => "загальна терапія",
-                2 or 3 => "хірургія",
-                4 or 5 => "кардіологія",
-                6 or 7 => "неврологія",
-                8 or 9 => "офтальмологія",
-                _ => "невідоме відділення"
-            };
-            // визначаємо пільгу та огляд за допомогою умовного (тернарного) оператора ?:
-            string piga = (num % 2 == 0) ? "так" : "ні";
-            string oglyad = (num % 3 == 0) ? "так" : "ні";
+                int k = int.Parse(Console.ReadLine()!);
+                doctors[i] = new int[k];
+                for (int j = 0; j < k; j++)
+                {
+                    doctors[i][j] = int.Parse(Console.ReadLine()!);
+                }
+            }
 
-            // виводимо три незалежні характеристики у три рядки за шаблоном
-            Console.WriteLine($"Відділення: {dep}");
-            Console.WriteLine($"Пільгова:   {piga}");
-            Console.WriteLine($"Огляд:      {oglyad}");
+            int bestDoctorIndex = 0;
+            int maxTotalIncome = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = doctors[i].Length;
+                int sum = 0;
+                for (int j = 0; j < count; j++)
+                {
+                    sum += doctors[i][j];
+                }
+
+                double average = count > 0 ? (double)sum / count : 0;
+
+                Console.WriteLine($"Лікар {i + 1}: {count} прийоми, сума={sum} грн, середня={average:F2} грн");
+
+                if (sum > maxTotalIncome)
+                {
+                    maxTotalIncome = sum;
+                    bestDoctorIndex = i;
+                }
+            }
+
+            Console.WriteLine($"Найбільший дохід: Лікар {bestDoctorIndex + 1} ({maxTotalIncome} грн)");
         }
     }
 }
