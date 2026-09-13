cdc92d1b962c687c2d489b4ac65301508952041b
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-13 18:33:27 +0300

    Lab03 Task02

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-﻿using Lab02;
+﻿using System;
 
-System.Threading.Thread.CurrentThread.CurrentCulture =
-    System.Globalization.CultureInfo.InvariantCulture;
+namespace ClinicApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doc1 = new Doctor("Олег", "Сидоренко", "Кардіологія", "LIC-001", "0441234567", 8, 16);
+            Doctor doc2 = new Doctor("Наталія", "Мороз", "Неврологія", "LIC-002", "0442345678", 9, 18);
+            Doctor doc3 = new Doctor("Андрій", "Власенко", "Педіатрія");
 
-Task8.Run();
+            Console.WriteLine(doc1);
+            Console.WriteLine(doc2);
+            Console.WriteLine(doc3);
+        }
+    }
+}
