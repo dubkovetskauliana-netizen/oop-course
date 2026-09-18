74734d2b5245d6e08d6581e227759702b47e2478
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-18 14:34:48 +0300

    Lab03: Task01

---
+++ ClinicApp/Patient.cs
@@ -0,0 +1,76 @@
+﻿namespace ClinicApp;
+
+public class Patient
+{
+    private static int _nextId = 1;
+
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string BloodType { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+
+    public string FullName
+    {
+        get { return FirstName + " " + LastName; }
+    }
+
+    public int Age
+    {
+        get
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+
+    public bool IsAdult
+    {
+        get { return Age >= 18; }
+    }
+
+    public Patient() : this("Невідомий", "Пацієнт", new DateTime(2000, 1, 1), "Невідомо", "0000000000")
+    {
+    }
+
+    public Patient(string firstName, string lastName) : this(firstName, lastName, new DateTime(2000, 1, 1), "Невідомо", "0000000000")
+    {
+    }
+
+    public Patient(string firstName, string lastName, DateTime dob, string bloodType, string phone)
+    {
+        Id = _nextId++;
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dob;
+        BloodType = bloodType;
+        Phone = phone;
+        Email = "";
+    }
+
+    public string GetAgeCategory()
+    {
+        int age = Age;
+        if (age < 18)
+        {
+            return "дитина";
+        }
+        if (age < 60)
+        {
+            return "дорослий";
+        }
+        return "літній";
+    }
+
+    public override string ToString()
+    {
+        return "[" + Id + "] " + FullName + " | Вік: " + Age + " (" + GetAgeCategory() + ") | Кров: " + BloodType + " | Тел: " + Phone;
+    }
+}
--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ClinicApp;
+
+Patient p1 = new Patient("Іван", "Петренко", new DateTime(1985, 5, 12), "A+", "0501234567");
+Patient p2 = new Patient("Олена", "Коваль", new DateTime(1993, 10, 25), "B-", "0672345678");
+Patient p3 = new Patient("Максим", "Бойко", new DateTime(2010, 3, 14), "0+", "0933456789");
+Patient p4 = new Patient();
+Patient p5 = new Patient("Марія", "Ткач");
+
+Console.WriteLine(p1.ToString());
+Console.WriteLine(p2.ToString());
+Console.WriteLine(p3.ToString());
+Console.WriteLine(p4.ToString());
+Console.WriteLine(p5.ToString());
