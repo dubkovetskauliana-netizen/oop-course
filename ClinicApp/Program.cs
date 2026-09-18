4c3a96cd872617bbb4040abf40054a389d303538
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-18 14:56:23 +0300

    Lab03: Task06

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,16 +1,28 @@
 ﻿using ClinicApp;
 
-Appointment a1 = new Appointment(1, 1, new DateTime(2026, 5, 9, 10, 0, 0), 30);
-Appointment a2 = new Appointment(2, 2, new DateTime(2026, 5, 9, 11, 0, 0), 45);
-Appointment a3 = new Appointment(3, 3, new DateTime(2026, 5, 10, 9, 0, 0), 20);
+PatientManager pm = new PatientManager();
+pm.Add(new Patient("Іван", "Петренко", new DateTime(1985, 5, 12), "A+", "0501234567"));
+pm.Add(new Patient("Олена", "Коваль", new DateTime(1993, 10, 25), "B-", "0672345678"));
+pm.Add(new Patient("Максим", "Бойко", new DateTime(2010, 3, 14), "0+", "0933456789"));
 
-Console.WriteLine(a1.ToString());
-Console.WriteLine(a2.ToString());
-Console.WriteLine(a3.ToString());
+DoctorManager dm = new DoctorManager();
+dm.Add(new Doctor("Олег", "Сидоренко", "Кардіологія"));
+dm.Add(new Doctor("Наталія", "Мороз", "Неврологія"));
+dm.Add(new Doctor("Андрій", "Власенко", "Педіатрія"));
 
-Console.WriteLine("\n// Після Cancel та Complete:");
-a1.Cancel("Пацієнт не зміг прийти");
-a2.Complete();
+AppointmentManager am = new AppointmentManager(pm, dm);
 
-Console.WriteLine(a1.ToString());
-Console.WriteLine(a2.ToString());
+Console.WriteLine();
+am.Book(1, 1, new DateTime(2026, 5, 9, 10, 0, 0), 30);
+am.Book(2, 2, new DateTime(2026, 5, 9, 11, 0, 0), 45);
+am.Book(3, 3, new DateTime(2026, 5, 10, 9, 0, 0), 20);
+am.Book(99, 1, new DateTime(2026, 5, 10, 11, 0, 0), 30);
+
+Console.WriteLine("\nМайбутні записи:");
+am.DisplayList(am.GetUpcoming());
+
+Console.WriteLine();
+am.Cancel(1, "Пацієнт не зміг прийти");
+
+Console.WriteLine("\nЗаписи пацієнта #2:");
+am.DisplayList(am.GetByPatient(2));
