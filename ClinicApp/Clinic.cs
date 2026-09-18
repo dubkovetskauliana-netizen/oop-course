ac687780b192dfea739f16e33160d196e8cbd029
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-18 14:59:03 +0300

    Lab03: Task07

---
+++ ClinicApp/Clinic.cs
@@ -0,0 +1,54 @@
+﻿namespace ClinicApp;
+
+public class Clinic
+{
+    public string Name { get; }
+    public PatientManager Patients { get; }
+    public DoctorManager Doctors { get; }
+    public AppointmentManager Appointments { get; }
+
+    public Clinic(string name)
+    {
+        Name = name;
+        Patients = new PatientManager();
+        Doctors = new DoctorManager();
+        Appointments = new AppointmentManager(Patients, Doctors);
+    }
+
+    public void DisplaySchedule(DateTime date)
+    {
+        Console.WriteLine("=== Розклад на " + date.ToString("dd.MM.yyyy") + " ===");
+        Appointment[] dayAppointments = Appointments.GetByDate(date);
+        Appointments.DisplayList(dayAppointments);
+    }
+
+    public void GenerateReport()
+    {
+        Appointment[] upcoming = Appointments.GetUpcoming();
+        Doctor[] allDoctors = Doctors.GetAll();
+
+        Console.WriteLine("┌──────────────────────────────────────────┐");
+        Console.WriteLine("│ Звіт – " + Name.PadRight(34) + "│");
+        Console.WriteLine("├──────────────────────────────────────────┤");
+        Console.WriteLine("│  Пацієнтів:        " + Patients.Count.ToString().PadRight(22) + "│");
+        Console.WriteLine("│  Лікарів:          " + Doctors.Count.ToString().PadRight(22) + "│");
+        Console.WriteLine("│  Майбутніх записів: " + upcoming.Length.ToString().PadRight(21) + "│");
+        Console.WriteLine("├──────────────────────────────────────────┤");
+        Console.WriteLine("│  Навантаження лікарів (майбутні записи): │");
+
+        for (int i = 0; i < allDoctors.Length; i++)
+        {
+            int docAppointmentsCount = 0;
+            for (int j = 0; j < upcoming.Length; j++)
+            {
+                if (upcoming[j].DoctorId == allDoctors[i].Id)
+                {
+                    docAppointmentsCount++;
+                }
+            }
+            string docLine = "    " + allDoctors[i].FullName + " (" + allDoctors[i].Speciality + "): " + docAppointmentsCount + " записів";
+            Console.WriteLine("│ " + docLine.PadRight(41) + "│");
+        }
+        Console.WriteLine("└──────────────────────────────────────────┘");
+    }
+}
--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,28 +1,23 @@
 ﻿using ClinicApp;
 
-PatientManager pm = new PatientManager();
-pm.Add(new Patient("Іван", "Петренко", new DateTime(1985, 5, 12), "A+", "0501234567"));
-pm.Add(new Patient("Олена", "Коваль", new DateTime(1993, 10, 25), "B-", "0672345678"));
-pm.Add(new Patient("Максим", "Бойко", new DateTime(2010, 3, 14), "0+", "0933456789"));
+Clinic clinic = new Clinic("Медична Клініка");
 
-DoctorManager dm = new DoctorManager();
-dm.Add(new Doctor("Олег", "Сидоренко", "Кардіологія"));
-dm.Add(new Doctor("Наталія", "Мороз", "Неврологія"));
-dm.Add(new Doctor("Андрій", "Власенко", "Педіатрія"));
+clinic.Patients.Add(new Patient("Іван", "Петренко", new DateTime(1985, 5, 12), "A+", "0501234567"));
+clinic.Patients.Add(new Patient("Олена", "Коваль", new DateTime(1993, 10, 25), "B-", "0672345678"));
+clinic.Patients.Add(new Patient("Максим", "Бойко", new DateTime(2010, 3, 14), "0+", "0933456789"));
+clinic.Patients.Add(new Patient("Марія", "Ткач", new DateTime(2000, 1, 1), "Невідомо", "0000000000"));
 
-AppointmentManager am = new AppointmentManager(pm, dm);
-
-Console.WriteLine();
-am.Book(1, 1, new DateTime(2026, 5, 9, 10, 0, 0), 30);
-am.Book(2, 2, new DateTime(2026, 5, 9, 11, 0, 0), 45);
-am.Book(3, 3, new DateTime(2026, 5, 10, 9, 0, 0), 20);
-am.Book(99, 1, new DateTime(2026, 5, 10, 11, 0, 0), 30);
-
-Console.WriteLine("\nМайбутні записи:");
-am.DisplayList(am.GetUpcoming());
+clinic.Doctors.Add(new Doctor("Олег", "Сидоренко", "Кардіологія"));
+clinic.Doctors.Add(new Doctor("Наталія", "Мороз", "Неврологія"));
+clinic.Doctors.Add(new Doctor("Андрій", "Власенко", "Педіатрія"));
 
 Console.WriteLine();
-am.Cancel(1, "Пацієнт не зміг прийти");
+clinic.Appointments.Book(1, 1, new DateTime(2027, 5, 9, 10, 0, 0), 30);
+clinic.Appointments.Book(2, 2, new DateTime(2027, 5, 9, 11, 0, 0), 45);
+clinic.Appointments.Book(3, 3, new DateTime(2027, 5, 10, 9, 0, 0), 20);
 
-Console.WriteLine("\nЗаписи пацієнта #2:");
-am.DisplayList(am.GetByPatient(2));
+Console.WriteLine();
+clinic.DisplaySchedule(new DateTime(2027, 5, 9));
+
+Console.WriteLine();
+clinic.GenerateReport();
