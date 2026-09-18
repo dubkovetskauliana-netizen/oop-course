a9bf22a5ee674937d1649348654b99e77c0d3866
dubkovetskauliana-netizen <dubkovetska.uliana@chnu.edu.ua>
2026-09-18 15:01:46 +0300

    Lab03: Task08

---
+++ ClinicApp/GrowablePatientManager.cs
@@ -0,0 +1,90 @@
+﻿namespace ClinicApp;
+
+public class GrowablePatientManager
+{
+    private Patient[] _patients = new Patient[4];
+    private int _count = 0;
+
+    public int Count
+    {
+        get { return _count; }
+    }
+
+    public int Capacity
+    {
+        get { return _patients.Length; }
+    }
+
+    private void Resize()
+    {
+        int oldCapacity = _patients.Length;
+        int newCapacity = oldCapacity * 2;
+        Patient[] temp = new Patient[newCapacity];
+
+        for (int i = 0; i < _count; i++)
+        {
+            temp[i] = _patients[i];
+        }
+
+        _patients = temp;
+        Console.WriteLine("Масив заповнений! Розширення: " + oldCapacity + " -> " + newCapacity);
+    }
+
+    public void Add(Patient patient)
+    {
+        if (_count == _patients.Length)
+        {
+            Resize();
+        }
+        _patients[_count] = patient;
+        _count++;
+        Console.WriteLine("Додано [" + patient.Id + "]. Розмір: " + _count + " / " + _patients.Length);
+    }
+
+    public Patient? FindById(int id)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            if (_patients[i].Id == id)
+            {
+                return _patients[i];
+            }
+        }
+        return null;
+    }
+
+    public bool Remove(int id)
+    {
+        int index = -1;
+        for (int i = 0; i < _count; i++)
+        {
+            if (_patients[i].Id == id)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            return false;
+        }
+
+        for (int i = index; i < _count - 1; i++)
+        {
+            _patients[i] = _patients[i + 1];
+        }
+
+        _count--;
+        _patients[_count] = null!;
+        return true;
+    }
+
+    public void DisplayAll()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            Console.WriteLine(_patients[i].ToString());
+        }
+    }
+}
--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,23 +1,36 @@
 ﻿using ClinicApp;
 
-Clinic clinic = new Clinic("Медична Клініка");
+Console.WriteLine("=== Тест GrowablePatientManager ===");
+Console.WriteLine("Додаємо пацієнтів одного за одним ...");
 
-clinic.Patients.Add(new Patient("Іван", "Петренко", new DateTime(1985, 5, 12), "A+", "0501234567"));
-clinic.Patients.Add(new Patient("Олена", "Коваль", new DateTime(1993, 10, 25), "B-", "0672345678"));
-clinic.Patients.Add(new Patient("Максим", "Бойко", new DateTime(2010, 3, 14), "0+", "0933456789"));
-clinic.Patients.Add(new Patient("Марія", "Ткач", new DateTime(2000, 1, 1), "Невідомо", "0000000000"));
+GrowablePatientManager gpm = new GrowablePatientManager();
 
-clinic.Doctors.Add(new Doctor("Олег", "Сидоренко", "Кардіологія"));
-clinic.Doctors.Add(new Doctor("Наталія", "Мороз", "Неврологія"));
-clinic.Doctors.Add(new Doctor("Андрій", "Власенко", "Педіатрія"));
+for (int i = 1; i <= 20; i++)
+{
+    gpm.Add(new Patient("Тест", "Пацієнт" + i));
+}
 
-Console.WriteLine();
-clinic.Appointments.Book(1, 1, new DateTime(2027, 5, 9, 10, 0, 0), 30);
-clinic.Appointments.Book(2, 2, new DateTime(2027, 5, 9, 11, 0, 0), 45);
-clinic.Appointments.Book(3, 3, new DateTime(2027, 5, 10, 9, 0, 0), 20);
+Console.WriteLine("\nТест пошуку:");
+Patient? found1 = gpm.FindById(10);
+if (found1 != null)
+{
+    Console.WriteLine("FindById(10) -> " + found1.FullName);
+}
+else
+{
+    Console.WriteLine("FindById(10) -> не знайдено");
+}
 
-Console.WriteLine();
-clinic.DisplaySchedule(new DateTime(2027, 5, 9));
+Patient? found2 = gpm.FindById(99);
+if (found2 != null)
+{
+    Console.WriteLine("FindById(99) -> " + found2.FullName);
+}
+else
+{
+    Console.WriteLine("FindById(99) -> не знайдено");
+}
 
-Console.WriteLine();
-clinic.GenerateReport();
+Console.WriteLine("\nПорівняння:");
+Console.WriteLine("  PatientManager:       100 місць (фіксовано)");
+Console.WriteLine("  GrowablePatientManager: " + gpm.Capacity + " місця (зросте при потребі)");
